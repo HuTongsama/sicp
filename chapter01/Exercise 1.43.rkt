2a453a281604d89fe70322d517afa2bcb561a149
hutongsama <hutongsama@163.com>
2019-02-05 23:04:31 +0800

    add exercise 1.42-1.45

---
+++ chapter01/Exercise 1.43.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+(define (compose f g)
+  (lambda (x) (f (g x))))
+(define (repeated f n)
+  (define (iter g count)
+    (if (= count 1)
+        g
+        (iter (compose f g) (- count 1))))
+  (iter f n))
+(define (square x)
+  (* x x))
+((repeated square 2) 5)
