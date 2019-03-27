42c6c4affe41f7211243b81d8785ebf245ef5355
hutongsama <542070807@qq.com>
2019-03-27 23:05:39 +0800

    add exercise2.32-2.34

---
+++ chapter02/Exercise 2.33.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+(define (length sequence)
+  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
+(define list1 (list 1 2 3 4 5))
+(define list2 (list 2 4 6 8))
+(map (lambda (x) (* x x)) list1)
+(append list1 list2)
+(length list1)
