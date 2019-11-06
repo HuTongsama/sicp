ab01d3501fde5e639a6ec3ee6017f93475495263
hutongsama <542070807@qq.com>
2019-11-06 23:01:44 +0800

    add exercise2.85-2.86

--- chapter02/Exercise 2.85.rkt
+++ chapter02/Exercise 2.85.rkt
@@ -12,11 +12,19 @@
                           (attach-tag 'integer (quotient (numer x) (denom x)))))
 ;integer package
 (put 'project 'integer (lambda (x)
-                         (error "This type can not be projected")))
+                         '()))
 (define (drop x)
   (define (can-be-dropped? x)
     (let ((projected-num ((get 'project (type-tag x)) (contents x))))
-      (let ((rised-num ((get 'rise (type-tag projected-num) (contents projected-num)))))
-        (if ((get 'equ (type-tag x)) rised-num projected-num)
-            #t
-            #f)))))
+      (if (not (null? projected-num))
+          (let ((rised-num ((get 'rise (type-tag projected-num) (contents projected-num)))))
+            (if ((get 'equ (type-tag x)) rised-num projected-num)
+                #t
+                #f))
+          #f)))
+  (if (can-be dropped? x)
+      (drop ((get 'project (type-tag x)) (contents x)))
+      x))
+;if we use drop in apply-generic,when two types are different
+;when can first to try to drop the higner type,if failed,then
+;we continue to rise the lower type
---
+++ chapter02/Exercise 2.86.rkt
@@ -0,0 +1,5 @@
+#lang sicp
+;need to define some generic mathematical procedure in
+;these packages which could be complex number's real part
+;,image part, magnitude or angle to support some calculate
+;in complex package
