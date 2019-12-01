7cd382ec4750d13d04bbda62abe5adfd64c67582
hutongsama <542070807@qq.com>
2019-12-01 21:39:55 +0800

    add exercise 3.9-3.11

---
+++ chapter03/Exercise 3.10.rkt
@@ -0,0 +1,4 @@
+#lang sicp
+;omit the figures ,the environment structures differ for
+;the two versions.The second version has one more environment
+;than the first version
---
+++ chapter03/Exercise 3.11.rkt
@@ -0,0 +1,5 @@
+#lang sicp
+
+;acc keeps local state in its own environment.
+;the two accounts have different enviroment.
+;the global environment is shared,maybe and the code of procedure body
