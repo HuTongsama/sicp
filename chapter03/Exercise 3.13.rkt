e6a000290f905680fcc566fcdc76dbe4a86149d9
hutongsama <542070807@qq.com>
2019-12-02 23:25:04 +0800

    add exercise3.12-3.14

---
+++ chapter03/Exercise 3.13.rkt
@@ -0,0 +1,4 @@
+#lang sicp
+
+;the structure z is like a->b->c->a
+;(last-pair z) will cause infinite loop
---
+++ chapter03/Exercise 3.14.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+;the procedure mystery will get a reversed list of the input
+;argument
+;v:('a)
+;w:('d 'c 'b 'a)
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+(define v (list 'a 'b 'c 'd))
+(define w (mystery v))
+v
+w
